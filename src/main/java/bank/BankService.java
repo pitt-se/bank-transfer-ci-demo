77421309e131e98ceff9d06647b64fcef914e2f7
Nadine von Frankenberg <vonfrankenberg@pitt.edu>
2026-04-08 14:10:27 -0400

    Add comments for transfer funds

--- src/main/java/bank/BankService.java
+++ src/main/java/bank/BankService.java
@@ -1,7 +1,7 @@
 package bank;
 
 public class BankService {
-
+    // Transfer funds from a user to another user
     public void transfer(User from, User to, double amount) {
         if (from.getBalance() > amount) {
             from.withdraw(amount);
